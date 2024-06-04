bd986f0f2ab96ab3536a63b0b7cc6c815dd49980
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2024-06-04 22:08:04 +0300

    adding screen utils to doc app

--- lib/core/theming/app_colors.dart
+++ lib/core/theming/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 abstract class AppColors {
   static const Color grey = Color(0xFF757575);
   static const Color primary = Color(0xFF247CFF);
+  static const Color background = Color(0xFFFFFFFF);
 }
--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -1,5 +1,7 @@
 import 'package:doc_app/core/routing/app_router.dart';
+import 'package:doc_app/core/theming/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/routing/app_routes.dart';
 
@@ -8,9 +10,18 @@ class DocApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      initialRoute: AppRoutes.onBoarding,
-      onGenerateRoute: AppRouter.onGenerateRoute,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      child:  MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: AppColors.primary,
+          scaffoldBackgroundColor: AppColors.background,
+        ),
+        initialRoute: AppRoutes.onBoarding,
+        onGenerateRoute: AppRouter.onGenerateRoute,
+      ),
     );
   }
 }
