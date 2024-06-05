bf9a28fa7ca8417f543846b088a350fc7cbe9241
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2024-06-06 01:01:48 +0300

    just a fake commit

--- lib/features/on_boarding/ui/views/on_boarding_view.dart
+++ lib/features/on_boarding/ui/views/on_boarding_view.dart
@@ -5,6 +5,7 @@ class OnBoardingView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     return const Scaffold(
       body: Center(
         child: Text('OnBoarding'),
