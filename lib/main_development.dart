d7bcb08a518b04db8d6a7d8e65b250e7e5c7d94c
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2024-06-23 00:21:26 +0300

    Ensure screen size initialization to fix text visibility
    Added flutter_screenutil package and ensured screen size initialization
    in both main.dart and main_development.dart. This change addresses the issue where text is hidden in release mode. The main functions are now asynchronous to accommodate the await call for ScreenUtil.ensureScreenSize().

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/di/dependency_injection.dart';
 import 'doc_app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // to fix text is hidden in release mode
+  await ScreenUtil.ensureScreenSize();
   initGetIt();
   runApp(const DocApp());
 }
