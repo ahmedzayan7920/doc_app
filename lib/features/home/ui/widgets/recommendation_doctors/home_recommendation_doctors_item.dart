727ce5623ed1276cc6c2d37ef4495ac57152986e
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2024-06-28 21:17:08 +0300

    Improve UI for Home Recommendation Doctors Item
    This commit introduces the following improvements to the `HomeRecommendationDoctorsItem` widget:
    
    1. Replaced the static loading indicator with a `CustomRectangleLoadingShimmer` widget for a better loading experience.
    2. Improved the error handling by displaying the error message in a centered and fitted manner when an image fails to load.
    3. Formatted the code to follow the project's coding style and standards.

--- lib/features/home/ui/widgets/recommendation_doctors/home_recommendation_doctors_item.dart
+++ lib/features/home/ui/widgets/recommendation_doctors/home_recommendation_doctors_item.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:doc_app/core/widgets/custom_rectangle_loading_shimmer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -8,7 +9,8 @@ import '../../../data/models/specialty_response_model.dart';
 
 class HomeRecommendationDoctorsItem extends StatelessWidget {
   const HomeRecommendationDoctorsItem({
-    super.key, required this.doctor,
+    super.key,
+    required this.doctor,
   });
 
   final DoctorModel doctor;
@@ -18,19 +20,10 @@ class HomeRecommendationDoctorsItem extends StatelessWidget {
     return Row(
       children: [
         CachedNetworkImage(
-          imageUrl:
-              doctor.photo,
-              progressIndicatorBuilder: (context, url, downloadProgress) {
-              return Container(
-                  width: 110.w,
-                  height: 110.h,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.circular(12.0),
-                    color: Colors.white,
-                  ),
-                );
-            },
+          imageUrl: doctor.photo,
+          progressIndicatorBuilder: (context, url, downloadProgress) {
+            return const CustomRectangleLoadingShimmer(width: 110, height: 110);
+          },
           imageBuilder: (context, imageProvider) => Container(
             width: 110.w,
             height: 110.h,
@@ -45,42 +38,46 @@ class HomeRecommendationDoctorsItem extends StatelessWidget {
           ),
           errorWidget: (context, url, error) {
             return Container(
-                  width: 110.w,
-                  height: 110.h,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.circular(12.0),
-                    color: Colors.white,
-                  ),
-                  child: Center(
-                    child: FittedBox(child: Text(error.toString(), maxLines: 10,)),
-                  ),
-                );
+              width: 110.w,
+              height: 110.h,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(12.0),
+                color: Colors.white,
+              ),
+              child: Center(
+                child: FittedBox(
+                    child: Text(
+                  error.toString(),
+                  maxLines: 10,
+                )),
+              ),
+            );
           },
         ),
-      const HorizontalSpace(16),
-      Expanded(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              doctor.name,
-              style: AppTextStyles.boldDarkBlue18,
-              overflow: TextOverflow.ellipsis,
-            ),
-            const VerticalSpace(5),
-            Text(
-              '${doctor.degree} | ${doctor.phone}',
-              style: AppTextStyles.mediumGrey12,
-            ),
-            const VerticalSpace(5),
-            Text(
-               doctor.email,
-              style: AppTextStyles.mediumGrey12,
-            ),
-          ],
+        const HorizontalSpace(16),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                doctor.name,
+                style: AppTextStyles.boldDarkBlue18,
+                overflow: TextOverflow.ellipsis,
+              ),
+              const VerticalSpace(5),
+              Text(
+                '${doctor.degree} | ${doctor.phone}',
+                style: AppTextStyles.mediumGrey12,
+              ),
+              const VerticalSpace(5),
+              Text(
+                doctor.email,
+                style: AppTextStyles.mediumGrey12,
+              ),
+            ],
+          ),
         ),
-      ),
       ],
     );
   }
